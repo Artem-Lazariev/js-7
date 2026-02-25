4c3b080162d8b0b2174b823aaacf056eef78fb30
Artem-Lazariev <lazarievartem25@gmail.com>
2026-02-25 20:37:00 +0200

    Implement card management functions
    Added functionality to remove, insert, and update cards in the cards array.

--- index.js
+++ index.js
@@ -8,3 +8,29 @@ for(let i = 0 ; i <= arr.length - 1;i++){
 }
 console.log(str)
 console.log(arr.join(""))
+
+const cards = [
+  'Карточка-1',
+  'Карточка-2',
+  'Карточка-3',
+  'Карточка-4',
+  'Карточка-5',
+];
+const cardToRemove = 'Карточка-3';
+const removeIndex = cards.indexOf(cardToRemove);
+
+if (removeIndex !== -1) {
+  cards.splice(removeIndex, 1);
+}
+const cardToInsert = 'Карточка-6';
+const insertIndex = 2; 
+
+cards.splice(insertIndex, 0, cardToInsert);
+const cardToUpdate = 'Карточка-4';
+const updateIndex = cards.indexOf(cardToUpdate);
+
+if (updateIndex !== -1) {
+  cards.splice(updateIndex, 1, 'Оновлена-Карточка-4');
+}
+
+console.log(cards);
